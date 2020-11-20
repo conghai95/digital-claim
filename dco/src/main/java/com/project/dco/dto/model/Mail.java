21d8891d9daeac3d5a6086dcd4b4fff1e96993de
conghai luong <conghaibk95@gmail.com>
2020-11-20 10:34:17 +0700

    abc

--- dco/src/main/java/com/project/dco/dto/model/Mail.java
+++ dco/src/main/java/com/project/dco/dto/model/Mail.java
@@ -3,18 +3,24 @@ package com.project.dco.dto.model;
 import com.project.dco.dto.model.base.EntityBase;
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Data
 @Entity
-@Table(name = "mail")
+@Table(name = "mails")
 public class Mail extends EntityBase {
 
+    @Column(name = "mail_from")
     String from;
+    @Column(name = "mail_to")
     String to;
+    @Column(name = "mail_cc")
     String cc;
+    @Column(name = "mail_bcc")
     String bcc;
+    @Column(name = "mail_subject")
     String subject;
     String content;
 }
